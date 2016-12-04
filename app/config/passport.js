2f9002b1719dd03b1b80cfe0145da5517da5e6e6
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-12-04 17:35:50 +0100

    Update passport.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -5,47 +5,47 @@ var User = require('../models/users');
 var configAuth = require('./auth');
 
 module.exports = function (passport) {
-	passport.serializeUser(function (user, done) {
-		done(null, user.id);
+    passport.serializeUser(function (user, done) {
+	done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+	    done(err, user);
 	});
+    });
 
-	passport.deserializeUser(function (id, done) {
-		User.findById(id, function (err, user) {
-			done(err, user);
-		});
+    passport.use(new TwitterStrategy({
+        consumerKey: configAuth.twitterAuth.consumerKey,
+        consumerSecret: configAuth.twitterAuth.consumerSecret,
+	callbackURL: configAuth.twitterAuth.callbackURL
+    },
+    function (token, refreshToken, profile, done) {
+        process.nextTick(function () {
+	    User.findOne({ 'twitter.id': profile.id }, function (err, user) {
+		if (err) {
+		    return done(err);
+		}
+
+		if (user) {
+		    return done(null, user);
+		} else {
+		    var newUser = new User();
+
+		    newUser.twitter.id = profile.id;
+		    newUser.twitter.username = profile.username;
+		    newUser.twitter.displayName = profile.displayName;
+		    newUser.twitter.photo = profile._json.profile_image_url;
+
+		    newUser.save(function (err) {
+		        if (err) {
+			    throw err;
+			}
+
+			return done(null, newUser);
+		    });
+		}
+	    });
 	});
-
-	passport.use(new TwitterStrategy({
-		consumerKey: configAuth.twitterAuth.consumerKey,
-		consumerSecret: configAuth.twitterAuth.consumerSecret,
-		callbackURL: configAuth.twitterAuth.callbackURL
-	},
-	function (token, refreshToken, profile, done) {
-		process.nextTick(function () {
-			User.findOne({ 'twitter.id': profile.id }, function (err, user) {
-				if (err) {
-					return done(err);
-				}
-
-				if (user) {
-					return done(null, user);
-				} else {
-					var newUser = new User();
-
-					newUser.twitter.id = profile.id;
-					newUser.twitter.username = profile.username;
-					newUser.twitter.displayName = profile.displayName;
-					newUser.twitter.photo = profile._json.profile_image_url;
-
-					newUser.save(function (err) {
-						if (err) {
-							throw err;
-						}
-
-						return done(null, newUser);
-					});
-				}
-			});
-		});
-	}));
+     }));
 };
