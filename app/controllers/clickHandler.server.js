ca98fe49186c98ffdb912e1f6821132a314a9b71
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-12-04 17:18:24 +0100

    Update clickHandler.server.js

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -6,11 +6,11 @@ var async = require('async');
 
 function ClickHandler () {
 	
-	this.getBars = function (req, res) {
+    this.getBars = function (req, res) {
 		
-		req.session.search = req.params.location;
+	req.session.search = req.params.location;
 		    
-		var yelp = new Yelp({
+	var yelp = new Yelp({
             consumer_key: process.env.YELP_CONSUMER_KEY,
             consumer_secret: process.env.YELP_CONSUMER_SECRET,
             token: process.env.YELP_TOKEN_KEY,
@@ -48,54 +48,53 @@ function ClickHandler () {
                 	} else {
                 		
                 		var newBar = new Bar();
-						newBar.yelp.id = business.id;
-						newBar.yelp.name = business.name;
-					    newBar.yelp.image_url = business.image_url || 'img/buildings/default.png';
-						newBar.yelp.rating = business.rating;
-						newBar.yelp.review_count = business.review_count;
-					    newBar.yelp.display_address = business.location.display_address;
+				newBar.yelp.id = business.id;
+				newBar.yelp.name = business.name;
+				newBar.yelp.image_url = business.image_url || 'img/buildings/default.png';
+				newBar.yelp.rating = business.rating;
+				newBar.yelp.review_count = business.review_count;
+				newBar.yelp.display_address = business.location.display_address;
 							
-						newBar.save(function (err) {
-					        if (err) callback('could not insert bar in the db');
+				newBar.save(function (err) {
+			            if (err) callback('could not insert bar in the db');
 							    
-							newBar = newBar.toObject();
-							newBar['sessionUser'] = { isVisiting: false };
-						    foundBars.push(newBar);
-						    callback();
-					    });
-                    }
+				    newBar = newBar.toObject();
+				    newBar['sessionUser'] = { isVisiting: false };
+				    foundBars.push(newBar);
+				    callback();
+			        });
+                        }
                 });
-			}, function (err) {
-				if (err) console.error('the async module has encountered a problem:', err);
-				res.send(foundBars);
-			});
+	    }, function (err) {
+	        if (err) console.error('the async module has encountered a problem:', err);
+		res.send(foundBars);
+	    });
         })
         .catch(function (err) {
-        	console.error('could not search yelp:', err);
+            console.error('could not search yelp:', err);
             res.send(err);
         });
-	};
+    };
 
-	this.visitBar = function (req, res) {
+    this.visitBar = function (req, res) {
 		
 		var id = req.params.id;
 		    
 		var query = { 'yelp.id': id, 'nightlife.visitor_ids': { $ne: req.user.twitter.id } };
 		var update = {
 		    $addToSet: { 'nightlife.visitor_ids': req.user.twitter.id }, 
-			$inc: { 'nightlife.visit_count': 1 }
+		    $inc: { 'nightlife.visit_count': 1 }
 		};
 	    	
-		Bar.
-			findOneAndUpdate(query, update, { new: true }, function (err, bar) {
-				if (bar) {
-					res.send(bar.nightlife);
-				} else {
-					if (err) console.error(err);
-                	res.send({ error: 'could not update bar in the db' });
-                }
-			});
-	};
+		Bar.findOneAndUpdate(query, update, { new: true }, function (err, bar) {
+		    if (bar) {
+			res.send(bar.nightlife);
+		    } else {
+			if (err) console.error(err);
+                        res.send({ error: 'could not update bar in the db' });
+                    }
+		});
+    };
 	
 	this.leaveBar = function (req, res) {
 		
@@ -107,17 +106,15 @@ function ClickHandler () {
 			$inc: { 'nightlife.visit_count': -1 }
 		};
 	    	
-		Bar.
-			findOneAndUpdate(query, update, { new: true }, function (err, bar) {
-				if (bar) {
-					res.send(bar.nightlife);
-				} else {
-					if (err) console.error(err);
+		Bar.findOneAndUpdate(query, update, { new: true }, function (err, bar) {
+		    if (bar) {
+		        res.send(bar.nightlife);
+		    } else {
+		        if (err) console.error(err);
                 	res.send({ error: 'could not update bar in the db' });
-                }
-			});
+                    }
+		});
 	};
-
 }
 
 module.exports = ClickHandler;
