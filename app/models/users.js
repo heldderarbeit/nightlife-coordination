d30400b60f807f9a09fd73a61ad2d7314bfb8ebc
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-12-04 17:45:36 +0100

    Update users.js

--- app/models/users.js
+++ app/models/users.js
@@ -4,12 +4,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var User = new Schema({
-	twitter: {
-		id: String,
-		displayName: String,
-		username: String,
-		photo: String
-	}
+    twitter: {
+	id: String,
+	displayName: String,
+	username: String,
+	photo: String
+    }
 });
 
 module.exports = mongoose.model('User', User);
