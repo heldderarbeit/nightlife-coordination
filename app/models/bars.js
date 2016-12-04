55bc19289d07c2743da230cee5658185d88f878b
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-12-04 17:45:07 +0100

    Update bars.js

--- app/models/bars.js
+++ app/models/bars.js
@@ -4,18 +4,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Bar = new Schema({
-	yelp: {
-		id: String,
-		image_url: String,
-		name: String,
-		rating: Number,
-		review_count: Number,
-		display_address: [String]
-	},
-   nightlife: {
-      visit_count: {type: Number, default: 0},
-      visitor_ids: [String]
-   }
+    yelp: {
+	id: String,
+	image_url: String,
+	name: String,
+	rating: Number,
+	review_count: Number,
+	display_address: [String]
+    },
+    nightlife: {
+        visit_count: {type: Number, default: 0},
+        visitor_ids: [String]
+    }
 });
-
 module.exports = mongoose.model('Bar', Bar);
